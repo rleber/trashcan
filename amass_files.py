593ba0aed9f84719982ea8acc6bdd2954ffe19c7
Richard LeBer <richard.leber@gmail.com>
2026-08-07 16:37:36 -0400

    Remove test code from amass_files

--- amass_files.py
+++ amass_files.py
@@ -1,7 +1,3 @@
 import log
 import logging
 log.start_logging(app = "amass_files",)
-logging.info("hello world")
-logging.info({"foo": "bar"})
-logging.info(["baz bat", {"gnu": "elk", "moose": "jaw"}])
-print("hello world")
